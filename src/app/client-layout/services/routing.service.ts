014d83601cba816c7d692eed3b8ac0b373b7ffca
IbrahimShafiq4 <hima.shafiq@gmail.com>
2025-05-23 12:59:32 +0300

    routing service updated

--- src/app/client-layout/services/routing.service.ts
+++ src/app/client-layout/services/routing.service.ts
@@ -31,6 +31,7 @@ export class RoutingService {
   }
 
   private checkIfRouteIsActive(url: string): boolean {
+    console.log(url)
     const cleanUrl = url.split('?')[0].split('#')[0];
     
     const segments = cleanUrl.split('/').filter(segment => segment !== '');
