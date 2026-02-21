d109b7bc42621c56f44ffb3c9d13de399e46767b
IbrahimShafiq4 <hima.shafiq@gmail.com>
2026-02-21 15:05:09 +0200

    battle p7 updated

--- src/app/client-layout/components/structured-components/css-battle-p7/css-battle-p7.ts
+++ src/app/client-layout/components/structured-components/css-battle-p7/css-battle-p7.ts
@@ -37,23 +37,7 @@ No images. No extra elements. Just CSS precision.
   HTMLCodeSnippets: ICodeStructure[] = [
     {
       code: `
-<p>
-<style>
-*{
-  background:#F7CB71;
-  color:7C3219;
-  +*{
-    border-block:32q solid;
-    margin:90 140;
-    border-radius:32q
-  }
-}
-  p{
-    padding:20+60;
-    margin:10-0;
-    box-shadow:0 95q,0-95q,0 0 0 22q inset
-  }
-</style>
+<p><style>*{background:#F7CB71;color:7C3219;+*{border-block:32q solid;margin:90 140;border-radius:32q}}p{padding:20+60;margin:10-0;box-shadow:0 95q,0-95q,0 0 0 22q inset}</style>
       `,
       codeTitle: 'index.html'
     }
